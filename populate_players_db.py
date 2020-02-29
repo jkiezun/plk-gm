5f754c7275a307cc39dd8c6540636ece130f6cde
Jakub Kiezun <kiezunjakub@gmail.com>
2020-02-29 12:12:37 +0100

    populate db script doesnt add duplicates now

--- populate_players_db.py
+++ populate_players_db.py
@@ -3,14 +3,15 @@ from fantasy.models import Player, Club
 with open('players.json') as f:
     data = json.load(f)
     for d in data['players']:
-        if Club.objects.filter(name=d['current_club']):
-            club_name = Club.objects.get(name=d['current_club'])
-        else:
-            club_name = Club.objects.get(name="1liga")
-        Player.objects.create(
-            first_name=d['first_name'],
-            last_name=d['last_name'],
-            Club=club_name,
-            number=d['number'],
-            position=d['position']
-        )
+        if not Player.objects.filter(first_name = d['first_name'], last_name = d['last_name']):
+            if Club.objects.filter(name=d['current_club']):
+                club_name = Club.objects.get(name=d['current_club'])
+            else:
+                club_name = Club.objects.get(name="1liga")
+            Player.objects.create(
+                first_name=d['first_name'],
+                last_name=d['last_name'],
+                Club=club_name,
+                number=d['number'],
+                position=d['position']
+            )
